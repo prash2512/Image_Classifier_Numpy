071f7d5374cab476627aa633c81783d75aa41f56
Prashanth <prash.v.2512@gmail.com>
2018-08-04 17:19:06 +0530

    K-nearest-neighbors using numpy  for Cifar-10

---
+++ knn_main.py
@@ -0,0 +1,70 @@
+import random
+import numpy as np
+from data_utils import load_CIFAR10
+import matplotlib.pyplot as plt
+from classifiers import KNearestNeighbor
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+cifar10_dir = 'datasets/cifar-10-batches-py'
+try:
+   del X_train, y_train
+   del X_test, y_test
+   print('Clear previously loaded data.')
+except:
+   pass
+
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+# As a sanity check, we print out the size of the training and test data.
+print('Training data shape: ', X_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+def plot_samples():
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    num_classes = len(classes)
+    samples_per_class = 7
+    for y, cls in enumerate(classes):
+        idxs = np.flatnonzero(y_train == y)
+        idxs = np.random.choice(idxs, samples_per_class, replace=False)
+        for i, idx in enumerate(idxs):
+            plt_idx = i * num_classes + y + 1
+            plt.subplot(samples_per_class, num_classes, plt_idx)
+            plt.imshow(X_train[idx].astype('uint8'))
+            plt.axis('off')
+            if i == 0:
+                plt.title(cls)
+    plt.show()
+
+num_training = 5000
+mask = list(range(num_training))
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+num_test = 500
+mask = list(range(num_test))
+X_test = X_test[mask]
+y_test = y_test[mask]
+
+X_train = np.reshape(X_train, (X_train.shape[0], -1))
+X_test = np.reshape(X_test, (X_test.shape[0], -1))
+print(X_train.shape, X_test.shape)
+
+classifier = KNearestNeighbor()
+classifier.train(X_train, y_train)
+
+dists = classifier.compute_distances_no_loops(X_test)
+print(dists.shape)
+plt.imshow(dists, interpolation='none')
+plt.show()
+
+y_test_pred = classifier.predict_labels(dists,y_test, k=4)
+
+# Compute and print the fraction of correctly predicted examples
+num_correct = np.sum(y_test_pred == y_test)
+accuracy = float(num_correct) / num_test
+print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
