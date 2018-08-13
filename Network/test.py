dfd2eee60f0b7a08f3eb878763a87eba11bec958
Prashanth <prash.v.2512@gmail.com>
2018-08-14 02:10:47 +0530

    Added backprop and forward prop for relu activation and tests for the same

--- Network/test.py
+++ Network/test.py
@@ -53,3 +53,30 @@ print('Testing affine_backward function:')
 print('dx error: ', rel_error(dx_num, dx)<=1e-9)
 print('dw error: ', rel_error(dw_num, dw)<=1e-9)
 print('db error: ', rel_error(db_num, db)<=1e-9)
+
+
+# Test the relu_forward function
+
+x = np.linspace(-0.5, 0.5, num=12).reshape(3, 4)
+
+out, _ = relu_forward(x)
+correct_out = np.array([[ 0.,          0.,          0.,          0.,        ],
+                        [ 0.,          0.,          0.04545455,  0.13636364,],
+                        [ 0.22727273,  0.31818182,  0.40909091,  0.5,       ]])
+
+# Compare your output with ours. The error should be on the order of e-8
+print('Testing relu_forward function:')
+print('difference: ', rel_error(out, correct_out))
+
+np.random.seed(231)
+x = np.random.randn(10, 10)
+dout = np.random.randn(*x.shape)
+
+dx_num = eval_numerical_gradient_array(lambda x: relu_forward(x)[0], x, dout)
+
+_, cache = relu_forward(x)
+dx = relu_backward(dout, cache)
+
+# The error should be on the order of e-12
+print('Testing relu_backward function:')
+print('dx error: ', rel_error(dx_num, dx))
