874de6219e95bbe84a442da43b6b9dfff58c7680
Prashanth <prash.v.2512@gmail.com>
2018-08-14 02:01:07 +0530

    Added forward and backward pass for affine layer and test to evaluate correctness

--- Network/layers.py
+++ Network/layers.py
@@ -20,16 +20,10 @@ def affine_forward(x, w, b):
     - out: output, of shape (N, M)
     - cache: (x, w, b)
     """
-    out = None
-    ###########################################################################
-    # TODO: Implement the affine forward pass. Store the result in out. You   #
-    # will need to reshape the input into rows.                               #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    cache = (x, w, b)
+
+    flatten_x = x.reshape(x.shape[0],-1)
+    out = np.dot(flatten_x,w)+b
+    cache = (x,w,b)
     return out, cache
 
 
@@ -50,14 +44,12 @@ def affine_backward(dout, cache):
     - db: Gradient with respect to b, of shape (M,)
     """
     x, w, b = cache
-    dx, dw, db = None, None, None
-    ###########################################################################
-    # TODO: Implement the affine backward pass.                               #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+    input_shape = x.shape
+    flatten_x = x.reshape(x.shape[0],-1)
+    print(dout.shape,x.shape,w.shape,b.shape)
+    dx = np.dot(dout,w.T).reshape(*input_shape)
+    db = np.sum(dout,axis=0)/x.shape[0]
+    dw = np.dot(flatten_x.T,dout)
     return dx, dw, db
 
 
--- Network/test.py
+++ Network/test.py
@@ -5,8 +5,14 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from layers import *
+from data_utils import get_CIFAR10_data
+from gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
 
 
+def rel_error(x,y):
+    x = x.reshape(np.prod(x.shape))
+    y = y.reshape(np.prod(y.shape))
+    return np.sum(x-y)
 # Test the affine_forward function
 
 num_inputs = 2
@@ -16,7 +22,7 @@ output_dim = 3
 input_size = num_inputs * np.prod(input_shape)
 weight_size = output_dim * np.prod(input_shape)
 
-x = np.linspace(-0.1, 0.5, num=input_size).reshape(num_inputs, *input_shape)
+x = np.linspace(-0.1, 0.5, num=input_size).reshape(num_inputs,*input_shape)
 w = np.linspace(-0.2, 0.3, num=weight_size).reshape(np.prod(input_shape), output_dim)
 b = np.linspace(-0.3, 0.1, num=output_dim)
 
@@ -26,4 +32,24 @@ correct_out = np.array([[ 1.49834967,  1.70660132,  1.91485297],
 
 # Compare your output with ours. The error should be around e-9 or less.
 print('Testing affine_forward function:')
-print('difference: ', rel_error(out, correct_out))
+print(rel_error(out, correct_out)<1e-8)
+
+np.random.seed(231)
+x = np.random.randn(10, 2, 3)
+w = np.random.randn(6, 5)
+b = np.random.randn(5)
+dout = np.random.randn(10, 5)
+
+dx_num = eval_numerical_gradient_array(lambda x: affine_forward(x, w, b)[0], x, dout)
+dw_num = eval_numerical_gradient_array(lambda w: affine_forward(x, w, b)[0], w, dout)
+db_num = eval_numerical_gradient_array(lambda b: affine_forward(x, w, b)[0], b, dout)
+
+_, cache = affine_forward(x, w, b)
+dx, dw, db = affine_backward(dout, cache)
+
+
+# The error should be around e-10 or less
+print('Testing affine_backward function:')
+print('dx error: ', rel_error(dx_num, dx)<=1e-9)
+print('dw error: ', rel_error(dw_num, dw)<=1e-9)
+print('db error: ', rel_error(db_num, db)<=1e-9)
